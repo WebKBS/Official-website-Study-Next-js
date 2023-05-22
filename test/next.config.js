3f3d2f7847d7eac2899e30bc9fcd2c76251dbb66
강범수 <kbs21c1987@gmail.com>
2023-05-22 18:48:20 +0900

    T

--- test/next.config.js
+++ test/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig: nextConfig,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
---
+++ test/pages/about.js
@@ -0,0 +1,6 @@
+import style from './about.module.css';
+const Post = () => {
+  return <h1 className={style.h1}>Hell!</h1>;
+};
+
+export default Post;
