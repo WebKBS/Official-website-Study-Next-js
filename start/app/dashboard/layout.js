ede92a5dcb80b4e0441e094cc561471d91bf4792
강범수 <kbs21c1987@gmail.com>
2023-05-23 19:02:42 +0900

    layout

---
+++ start/app/dashboard/layout.js
@@ -0,0 +1,10 @@
+const DashboardLayout = ({ children }) => {
+  return (
+    <section>
+      <nav>네비게이션~~~~~~~~~~~~~~~~~~~~~~~~~~~</nav>
+      {children}
+    </section>
+  );
+};
+
+export default DashboardLayout;
